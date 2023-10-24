1dc94ae5f3956040b61a8b99fdc3845c5ccbc971
Haeun Kim <53562331+haeunk1@users.noreply.github.com>
2023-10-24 13:41:58 +0900

    Update vue
    api통신을 이용한 카드 데이터 수정 테스트

--- gallery/frontend/src/components/Card.vue
+++ gallery/frontend/src/components/Card.vue
@@ -8,8 +8,7 @@
                 <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
             </svg>
             <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.</p>
+                <p class="card-text">{{item}}</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -23,7 +22,10 @@
 
 <script>
 export default {
-    name: 'Card'
+    name: 'Card',
+    props:{
+        item: String //타입지정
+    }
 }
 </script>
 
--- gallery/frontend/src/pages/Home.vue
+++ gallery/frontend/src/pages/Home.vue
@@ -4,7 +4,9 @@
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
                     <h1 class="fw-light">Album example</h1>
-                    <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                    <p class="lead text-body-secondary">Something short and leading about the collection below—its
+                        contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply
+                        skip over it entirely.</p>
                     <p>
                         <a href="#" class="btn btn-primary my-2">Main call to action</a>
                         <a href="#" class="btn btn-secondary my-2">Secondary action</a>
@@ -15,10 +17,9 @@
 
         <div class="album py-5 bg-body-tertiary">
             <div class="container">
-
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col" v-for="i in 12" :key="i">
-                        <Card/>
+                    <div class="col" v-for="item in state.items" :key="item">
+                        <Card :item="item"/>
                     </div>
                 </div>
             </div>
@@ -28,10 +29,22 @@
 
 <script>
 import Card from "@/components/Card.vue";
+import axios from "axios";
+import {reactive} from "vue";
 
-export default{
-    name:"Home",
-    components: {Card}
+export default {
+    name: "Home",
+    components: {Card},
+    setup() {
+        const state = reactive({
+            items: []
+        })
+        axios.get("/api/items").then(({data}) => { //res -> {data} 요소를 바로 꺼내옴
+            state.items = data;
+        })
+
+        return {state}
+    }
 }
 </script>
 
